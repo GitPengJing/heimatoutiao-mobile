81c39e4c8a13dc40078cdf52382a9f0c78db6a45
PengBillion <954483281@qq.com>
2020-03-15 21:21:00 +0800

    路由规则及配置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import 'amfe-flexible' // 根据屏幕大小的改变 改变屏幕font-size的大小
 import Vant from 'vant' // 引入vant组件
 import 'vant/lib/index.css' // 引入vant 样式
+import '@/styles/index.less' // 引入全局自定义样式
 Vue.use(Vant) // 全局注册
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,65 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-const home = () => import('@/views/home')
+
+const layout = () => import('@/views/layout') // 按需加载 layout
+const home = () => import('@/views/home') // 按需加载 home 二级路由
+const question = () => import('@/views/question')// 按需加载 question 二级路由
+const video = () => import('@/views/video')// 按需加载 video 二级路由
+const user = () => import('@/views/user')// 按需加载 user 二级路由
+
+const chat = () => import('@/views/user/chat') // 按需加载小智同学
+const profile = () => import('@/views/user/profile') // 编辑资料
+const article = () => import('@/views/article') // 文章详情
+const login = () => import('@/views/login') // 登录
+const search = () => import('@/views/search') // 搜索中心
+const result = () => import('@/views/search/result') // 搜索结果
 const routes = [
   {
-    path: '/',
-    component: home
+    path: '/', // 布局组件
+    component: layout,
+    children: [
+      {
+        path: '', // 首页组件
+        component: home
+      },
+      {
+        path: '/question', // 问答组件
+        component: question
+      },
+      {
+        path: '/video', // 视频组件
+        component: video
+      },
+      {
+        path: '/user', // 个人中心组件
+        component: user
+      }
+    ]
+  },
+  {
+    path: '/login', // 登录组件
+    component: login
+  },
+  {
+    path: '/user/chat', // 小智同学组件
+    component: chat
+  },
+  {
+    path: '/user/profile', // 编辑资料组件
+    component: profile
+  },
+  {
+    path: '/article', // 文章详情组件
+    component: article
+  },
+  {
+    path: '/search', // 搜索中心组件
+    component: search
+  },
+  {
+    path: '/search/result', // 搜索结果组件
+    component: result
   }
 ]
 
