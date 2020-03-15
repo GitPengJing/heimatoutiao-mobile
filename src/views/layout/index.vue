9440ea528dcace5a0837efd9025a2349b6ecf9c4
PengBillion <954483281@qq.com>
2020-03-15 22:05:29 +0800

    layout组件完成

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,20 @@
 <template>
-<div></div>
+  <div class="container">
+    <!-- 顶部导航 fixed固定在顶部 如果路由地址是/user就没有van-nav-bar-->
+    <van-nav-bar title="黑马头条" fixed right-text="搜索" v-if="$route.path!=='/user'">
+    </van-nav-bar>
+    <!-- 中间二级路由 如果地址是/user就没有padding-top-->
+    <div class="my-wrapper" :class="{noTop:$route.path==='/user'}">
+        <router-view></router-view>
+    </div>
+    <!-- 底部标签栏 route开启路由模式-->
+    <van-tabbar route>
+        <van-tabbar-item to='/' icon="wap-home-o">首页</van-tabbar-item>
+        <van-tabbar-item to='/question' icon="question-o">问答</van-tabbar-item>
+        <van-tabbar-item to='/video' icon="video-o">视频</van-tabbar-item>
+        <van-tabbar-item to='/user' icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
@@ -8,6 +23,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang='less'>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 </style>
