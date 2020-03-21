499d66e13f1bb03a3e210b79070e5187afc07972
PengBillion <954483281@qq.com>
2020-03-21 23:38:43 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import store from './store'
 import plugin from '@/utils/plugin' // 引入插件
 import '@/permission' // 导航守卫
 import 'amfe-flexible' // 根据屏幕大小的改变 改变屏幕font-size的大小
-import Vant from 'vant' // 引入vant组件
+import Vant, { Lazyload } from 'vant' // 引入vant组件
 import 'vant/lib/index.less' // 引入vant 样式
 import '@/styles/index.less' // 引入全局自定义样式
 Vue.use(Vant) // 全局注册
-Vue.use(plugin)
+Vue.use(plugin) // 全局注册plugin工具组件
+Vue.use(Lazyload) // 全局注册图片懒加载
 
 Vue.config.productionTip = false
 
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -12,12 +12,12 @@
               <h3 class="van-ellipsis">{{item.title}}</h3>
               <div class="img_box" v-if="item.cover.type === 3">
                   <!-- 根据封面类型觉得是三图 单图 还是无图 -->
-                <van-image class="w33" fit="cover" :src="item.cover.images[0]" />
-                <van-image class="w33" fit="cover" :src="item.cover.images[1]" />
-                <van-image class="w33" fit="cover" :src="item.cover.images[2]" />
+                <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[0]" />
+                <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[1]" />
+                <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[2]" />
               </div>
               <div class="img_box" v-if="item.cover.type === 1">
-                <van-image class="w100" fit="cover" :src="item.cover.images[0]" />
+                <van-image lazy-load class="w100" fit="cover" :src="item.cover.images[0]" />
               </div>
               <div class="info_box">
                 <span>{{item.aut_name}}</span>
