4e1c02261ad231cd108d695e69799d29f79cd151
PengBillion <954483281@qq.com>
2020-03-23 18:42:50 +0800

    搜索中心-搜索记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,21 +3,26 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
+    <van-search @search="onSearch" v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
+    <!-- 搜索建议 -->
     <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
+    <!-- 历史记录 -->
     <div class="history-box" v-else>
-      <div class="head">
+      <!-- 如果有历史记录才显示 -->
+      <div class="head" v-if="historyList.length">
         <span>历史记录</span>
-        <van-icon name="delete"></van-icon>
+        <van-icon @click="clearHistory" name="delete"></van-icon>
       </div>
       <van-cell-group>
-        <van-cell>
-          <a class="word_btn">电脑</a>
-          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        <!-- 点击搜索记录去搜索结果页 -->
+        <van-cell @click="toSearchRes(item)" v-for="(item,index) in historyList" :key="index">
+          <a class="word_btn">{{item}}</a>
+          <!-- .stop修饰符阻止冒泡事件 -->
+          <van-icon @click.stop="delHistory(index)" class="close_btn" slot="right-icon" name="cross" />
         </van-cell>
       </van-cell-group>
     </div>
@@ -25,10 +30,57 @@
 </template>
 
 <script>
+const key = 'hm-toutiao-mobile-history'
 export default {
   data () {
     return {
-      q: '' // 输入框输入的内容
+      q: '', // 输入框输入的内容
+      historyList: JSON.parse(localStorage.getItem(key) || '[]') // 存放搜索历史记录
+    }
+  },
+  methods: {
+    // 删除历史记录
+    delHistory (index) {
+      // 删除选中的记录
+      this.historyList.splice(index, 1)
+      // 删除之后同步到本地
+      localStorage.setItem(key, JSON.stringify(this.historyList))
+    },
+    // 跳到搜索结果页
+    toSearchRes (text) {
+      // query传参
+      this.$router.push({ path: '/search/result', query: { q: text } })
+    },
+    // 回车搜索
+    onSearch (q) {
+      // 判断输入框是否输入了内容
+      if (!this.q) return
+      // 将搜索内容加到搜索历史列表
+      this.historyList.push(this.q)
+      // 去重
+      // from通过给定的对象中创建一个数组。
+      // new Set 去重方法
+      this.historyList = Array.from(new Set(this.historyList))
+      // 同步到本地
+      localStorage.setItem(key, JSON.stringify(this.historyList))
+      // 跳转到搜索的内容页面
+      this.$router.push({ path: '/search/result', query: { q: this.q } })
+    },
+    // 清空搜索记录
+    async clearHistory () {
+      try {
+        // 提示是否确定清楚历史记录
+        await this.$dialog.confirm({
+          title: '提示',
+          message: '确定要清楚历史记录吗？'
+        })
+        // 清空历史记录列表
+        this.historyList = []
+        // 本地同步
+        localStorage.setItem(key, '[]')
+      } catch (error) {
+
+      }
     }
   }
 }
